1ee2e57c098ed9ae8cd72b81860a8c01534c249c
Joepock123 <joe.m.jackson@live.co.uk>
2022-05-29 15:38:37 +0100

    Remove pg package and files

--- pages/infrastructure/config.ts
+++
@@ -1,6 +0,0 @@
-// PG
-export const PGHOST = process.env.PGHOST
-export const PGPORT = process.env.PGPORT
-export const PGDATABASE = process.env.PGDATABASE
-export const PGUSER = process.env.PGUSER
-export const PGPASSWORD = process.env.PGPASSWORD
